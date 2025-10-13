933f87afc7e2255be6b3f0f3ea75283ca7d0fdc2
Tiger <tig.zhang@mail.utoronto.ca>
2025-10-12 20:30:39 -0400

    more testing

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -32,7 +32,7 @@ public class Main {
             return subBreeds.size();
         }
         catch (BreedFetcher.BreedNotFoundException e){
-            return 0;
+            return -1;
         }
     }
 }
